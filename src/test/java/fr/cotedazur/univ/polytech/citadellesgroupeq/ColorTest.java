bbe99044d2174c0f5c012a10b25c61245c0592c6
Gauthier MARTIN <gauthier.martin1@etu.unice.fr>
2023-12-14 16:43:09 +0100

    test: added test for winning coins for same color feature.

---
+++ src/test/java/fr/cotedazur/univ/polytech/citadellesgroupeq/ColorTest.java
@@ -0,0 +1,40 @@
+package fr.cotedazur.univ.polytech.citadellesgroupeq;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ColorTest {
+    GameManager game;
+    Player firstP, secondP;
+    @BeforeEach
+    void setup() {
+        game=new GameManager();
+        firstP=game.getPlayersList().get(0);
+        secondP=game.getPlayersList().get(1);
+    }
+
+    @Test
+    void testGrayDoesntWinCoins() {
+        firstP.setRole(Role.ASSASSIN);//arbitrary role with GRAY color
+        firstP.addCitadelToCity(new Citadel("temple", 8, Color.GRAY));
+        firstP.addCitadelToCity(new Citadel("autre", 9, Color.GRAY));
+        firstP.addCitadelToCity(new Citadel("autreencore", 9, Color.RED));//added another color to check
+
+        RoundSummary summary=game.playPlayerTurn(firstP);
+        assertEquals(0, summary.getCoinsWonByColorCards());
+    }
+
+    @Test
+    void testRedColorWinCoins() {//or any other color
+        firstP.setRole(Role.CONDOTTIERE);//condottiere is RED
+        firstP.addCitadelToCity(new Citadel("temple", 8, Color.GRAY));
+        firstP.addCitadelToCity(new Citadel("temple", 8, Color.RED));
+        firstP.addCitadelToCity(new Citadel("temple", 8, Color.BLUE));
+        firstP.addCitadelToCity(new Citadel("temple", 8, Color.RED));
+        firstP.addCitadelToCity(new Citadel("temple", 8, Color.PURPLE));//added some other colors to check it doesn't count other colors
+
+        RoundSummary summary=game.playPlayerTurn(firstP);
+        assertEquals(2, summary.getCoinsWonByColorCards());
+    }
+}
