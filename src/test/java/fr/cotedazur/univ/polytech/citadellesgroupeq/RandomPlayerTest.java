ba8aa3dd8b9a19ee50d398bbe239f1828bc39839
Gauthier MARTIN <45541503+martgauthier@users.noreply.github.com>
2024-01-05 17:38:24 +0100

    mergd master into kingpower + fixed little bug in RandomPlayerTest

--- src/test/java/fr/cotedazur/univ/polytech/citadellesgroupeq/RandomPlayerTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/citadellesgroupeq/RandomPlayerTest.java
@@ -5,25 +5,33 @@ import static org.mockito.Mockito.*;
 
 import fr.cotedazur.univ.polytech.citadellesgroupeq.gamelogic.GameLogicManager;
 import fr.cotedazur.univ.polytech.citadellesgroupeq.gamelogic.RoundSummary;
+import fr.cotedazur.univ.polytech.citadellesgroupeq.players.AlwaysSpendPlayer;
 import fr.cotedazur.univ.polytech.citadellesgroupeq.players.Player;
 import fr.cotedazur.univ.polytech.citadellesgroupeq.players.RandomPlayer;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.List;
 import java.util.Random;
 
 class RandomPlayerTest {
 
     Player randomPlayer;
+    Player otherPlayer;
     RoundSummary summary;
     Random trickedRandom;
+    GameLogicManager game;
 
     @BeforeEach
     void setUp() {
         summary = new RoundSummary();
         randomPlayer = new RandomPlayer(0);
+        otherPlayer=new AlwaysSpendPlayer(1);
+        otherPlayer.setRole(Role.CONDOTTIERE);
+        game=new GameLogicManager(List.of(randomPlayer, otherPlayer));
         if(trickedRandom != null) {//limits creation of new mocks
             reset(trickedRandom);
         }
@@ -34,20 +42,20 @@ class RandomPlayerTest {
 
     @Test
     void testPlayPlayerTurnRandomChoice1() {
-        randomPlayer.setRole(Role.ASSASSIN);
-        when(trickedRandom.nextInt(anyInt())).thenReturn(1);
+        randomPlayer.setRole(Role.ROI);
+        doReturn(1).when(trickedRandom).nextInt(anyInt());
         randomPlayer.setRandomGenerator(trickedRandom);
-        randomPlayer.playPlayerTurn(summary, new GameLogicManager());
+        randomPlayer.playPlayerTurn(summary, game);
         assertTrue(summary.hasPickedCash());
     }
 
 
     @Test
     void testPlayPlayerTurnRandomChoice0() {
-        randomPlayer.setRole(Role.ASSASSIN);
+        randomPlayer.setRole(Role.ROI);
         when(trickedRandom.nextInt(anyInt())).thenReturn(0);
         randomPlayer.setRandomGenerator(trickedRandom);
-        randomPlayer.playPlayerTurn(summary, new GameLogicManager());
+        randomPlayer.playPlayerTurn(summary, game);
         assertTrue(summary.hasPickedCards());
     }
 }
