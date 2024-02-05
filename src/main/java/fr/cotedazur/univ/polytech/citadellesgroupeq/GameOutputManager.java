b976ec019d8e87ed8022cbaf539ad54c39b9f4c2
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-02-05 15:21:40 +0100

    Refactor : Make the GameOutputManager using logÃ

--- src/main/java/fr/cotedazur/univ/polytech/citadellesgroupeq/GameOutputManager.java
+++ src/main/java/fr/cotedazur/univ/polytech/citadellesgroupeq/GameOutputManager.java
@@ -2,17 +2,20 @@ package fr.cotedazur.univ.polytech.citadellesgroupeq;
 
 import fr.cotedazur.univ.polytech.citadellesgroupeq.gamelogic.GameLogicManager;
 import fr.cotedazur.univ.polytech.citadellesgroupeq.gamelogic.RoundSummary;
+import fr.cotedazur.univ.polytech.citadellesgroupeq.loggerformat.SimpleFormatterWithoutDate;
 import fr.cotedazur.univ.polytech.citadellesgroupeq.players.Player;
 
 import java.util.AbstractMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.*;
 
-@SuppressWarnings("java:S106")//asks for printing with a logger instead of System.out.println
+
 public class GameOutputManager {
     private GameLogicManager game;
     private int rounds;
     private int playersNumber;
+    private static final Logger GAMEPLAY_LOGGER = Logger.getLogger("Gameplay_Logger");
 
     public GameOutputManager() {
         game=new GameLogicManager();
@@ -26,55 +29,71 @@ public class GameOutputManager {
         playersNumber=playerList.size();
     }
 
+    public void setupLogger(Level newLevel){
+        GAMEPLAY_LOGGER.setLevel(newLevel);
+        GAMEPLAY_LOGGER.getParent().getHandlers()[0].setFormatter(new SimpleFormatterWithoutDate());
+    }
+
     public void startMainOutputLoop() {
+        setupLogger(Level.INFO);
         while(!game.isFinished()) {//pour chaque manche
-            System.out.println("Tour : " + rounds ++ + "\n--------------\n");
+            GAMEPLAY_LOGGER.log(Level.INFO, "Tour : {}\n--------------\n", rounds++);
             List<Role> availableRoles = game.generateAvailableRoles(playersNumber);
 
             describeRoles(availableRoles);//liste les rôles rendus disponibles à choisir par le jeu
 
             describeRolePicking(game.makeAllPlayersSelectRole(availableRoles), game.getMasterOfTheGameIndex());//décrit les rôles choisis par chaque joueur
-
             for(Player player : game.getPlayerTreeSet()) {
                 describePlayerRound(player, game);
             }
 
 
             game.resuscitateAllPlayers();
-            System.out.println("--------------");
+            GAMEPLAY_LOGGER.info("--------------");
         }
-        System.out.println("Voici le score des joueurs qui n'ont pas gagné:");
+        GAMEPLAY_LOGGER.info("Voici le score des joueurs qui n'ont pas gagné:");
         for(Player player : game.getPlayersList()){
             if(player!=game.whoIsTheWinner()){
-                System.out.println("Le joueur "+player.getBotLogicName()+player.getStrategyName()+" a un score de "+game.getScoreOfEnd().get(player));
+                GAMEPLAY_LOGGER.log(Level.INFO,
+                        "Le joueur {} {}  a un score de {}",
+                        new Object[]{player.getBotLogicName(), player.getStrategyName(), game.getScoreOfEnd().get(player)}
+                );
             }
         }
-        System.out.println("Et enfin!! Le joueur "+game.whoIsTheWinner().getBotLogicName()+game.whoIsTheWinner().getStrategyName()+" a gagné avec un score de "+game.getScoreOfEnd().get(game.whoIsTheWinner()));
-        System.out.println("A noter pour le décompte des points, qu'il possédait ces merveilles: (rien si pas de cartes violettes)");
+        GAMEPLAY_LOGGER.log(
+                Level.INFO, "{} {} a gagné avec un score de {} ",
+                new Object[]{game.whoIsTheWinner().getBotLogicName(),game.whoIsTheWinner().getStrategyName(),game.getScoreOfEnd().get(game.whoIsTheWinner())}
+        );
+
+        GAMEPLAY_LOGGER.info("A noter pour le décompte des points, qu'il possédait ces merveilles: (rien si pas de cartes violettes)");
         for(District district: game.whoIsTheWinner().getCity()) {
             if(district.getColor() == Color.PURPLE) {
-                System.out.println("* " + district.getName());
+                GAMEPLAY_LOGGER.info("* " + district.getName());
             }
         }
-        System.out.println("Jeu fini !");
+        GAMEPLAY_LOGGER.info("Jeu fini !");
     }
 
 
     public void describeRoles(List<Role> listeRoles) {
-        System.out.println("Les roles disponibles sont: ");
+        GAMEPLAY_LOGGER.info("Les roles disponibles sont: ");
         for(Role role: listeRoles) {
-            System.out.print("\t-"+role.name());
+            GAMEPLAY_LOGGER.info("\t-"+role.name());
         }
-        System.out.println("\n");
+        GAMEPLAY_LOGGER.info("\n");
     }
 
     public void describeRolePicking(List<Player> players, int masterOfTheGameIndex) {
         for(Player player: players) {
-            System.out.print("Le joueur " + player.getBotLogicName()+player.getStrategyName());
+            GAMEPLAY_LOGGER.log(Level.INFO,
+                    "Le joueur {} {}", new Object[]{player.getBotLogicName(),player.getStrategyName()}
+            );
             if(player.getId() == masterOfTheGameIndex) {
-                System.out.print(" (maitre du jeu)");
+                GAMEPLAY_LOGGER.info(" (maitre du jeu)");
             }
-            System.out.println(" a choisi son role: " + player.getRole() + " ("+player.getRole().getColor().name()+")");
+            GAMEPLAY_LOGGER.log(Level.INFO, " a choisi son role: {} ({})",
+                    new Object[]{player.getRole(), player.getRole().getColor().name()}
+            );
         }
     }
 
@@ -84,78 +103,96 @@ public class GameOutputManager {
         RoundSummary summary = game.playPlayerTurn(player);
 
         if(summary.hasBeenKilled()){
-            System.out.println("Ce joueur a été tué par l'assassin, il ne peut donc pas effectuer son tour");
+            GAMEPLAY_LOGGER.info("Ce joueur a été tué par l'assassin, il ne peut donc pas effectuer son tour");
         }
         else {
             if (summary.hasUsedPower()) {
-                System.out.println("Ce joueur utilise son pouvoir de " + player.getRole().name());
+                GAMEPLAY_LOGGER.info("Ce joueur utilise son pouvoir de " + player.getRole().name());
                 if(player.getRole().equals(Role.ASSASSIN)) {
                     for (Player p : game.getPlayersList()) {
                         if (p.isDeadForThisTurn()) {
-                            System.out.println("et a tué le joueur " + p.getRole() + " de nom " + p.getBotLogicName()+p.getStrategyName());
+                            GAMEPLAY_LOGGER.log(Level.INFO, "et a tué le joueur {} de nom {} {}",
+                                    new Object[]{p.getRole(), p.getBotLogicName(), p.getStrategyName()}
+                            );
                             break;
                         }
                     }
                 }
                 else if(player.getRole().equals(Role.MAGICIEN)){
                     if(summary.hasExchangedCardsWithPileAsMagician()) {
-                        System.out.println("et décide d'échanger ses cartes avec la pioche et il a échangé "+summary.getExchangedCardsWithPileIndex().length+" cartes.");
+                        GAMEPLAY_LOGGER.log(Level.INFO,
+                                "et décide d'échanger ses cartes avec la pioche et il a échangé {} cartes.",
+                                summary.getExchangedCardsWithPileIndex().length
+                        );
                     }
                     else {
                         Player exchangedWith = game.getPlayersList().get(summary.getExchangedCardsPlayerId());
-                        System.out.println("et décide d'échanger ses cartes avec le joueur " + exchangedWith.getRole() + " qui est le joueur " + exchangedWith.getBotLogicName()+exchangedWith.getStrategyName());
+                        GAMEPLAY_LOGGER.log(Level.INFO,
+                                "et décide d'échanger ses cartes avec le joueur {} qui est le joueur {} {}",
+                                new Object[]{exchangedWith.getRole(),exchangedWith.getBotLogicName(), exchangedWith.getStrategyName()}
+                        );
                     }
                 }
                 else if(player.getRole().equals(Role.MARCHAND)){
-                    System.out.println("et gagne une pièce bonus");
+                    GAMEPLAY_LOGGER.info("et gagne une pièce bonus");
                 }
                 else if(player.getRole().equals(Role.VOLEUR)){
-                    System.out.println("et a volé le joueur " + summary.getStealedRole());
+                    GAMEPLAY_LOGGER.info("et a volé le joueur " + summary.getStealedRole());
                 }
                 else if (player.getRole() == Role.CONDOTTIERE && summary.getOptionalDestroyedDistrict().isPresent()) {
                     AbstractMap.SimpleEntry<Integer, District> districtDestroyed=summary.getOptionalDestroyedDistrict().get();
-                    System.out.println(" et a détruit le district " + districtDestroyed.getValue().getName() + " du joueur d'id " + districtDestroyed.getKey());
+                    GAMEPLAY_LOGGER.log(Level.INFO, " et a détruit le district {} du joueur d'id {}",
+                            new Object[]{districtDestroyed.getValue().getName(), districtDestroyed.getKey()}
+                    );
                 }
                 else if(player.getRole() == Role.ARCHITECTE) {
-                    System.out.println("Il a donc automatiquement gagné 2 cartes de plus.");
+                    GAMEPLAY_LOGGER.info("Il a donc automatiquement gagné 2 cartes de plus.");
                 }
             }
 
             if (summary.hasWonCoinsByColorCards()) {
-                System.out.println("Grâce à sa couleur et à ses cartes, il a gagné " + summary.getCoinsWonByColorCards() + " pièces.");
+                GAMEPLAY_LOGGER.log(Level.INFO,"Grâce à sa couleur et à ses cartes, il a gagné {} pièces.",
+                        summary.getCoinsWonByColorCards()
+                );
             }
             Optional<District> optionalEcoleDeMagie = player.getDistrictInCity("Ecole de magie");
             if (optionalEcoleDeMagie.isPresent()){
                 District ecoleDeMagie = optionalEcoleDeMagie.get();
                 if(ecoleDeMagie.getColor() == player.getRole().getColor()){
-                    System.out.println("Dont une pièce grâce à l'Ecole de magie");
+                    GAMEPLAY_LOGGER.info("Dont une pièce grâce à l'Ecole de magie");
                 }
             }
 
 
             if (summary.hasPickedCards()) {
-                System.out.println("Il a choisi de piocher 1 carte: " + summary.getDrawnCards().get(0).getName());
+                GAMEPLAY_LOGGER.log(Level.INFO,"Il a choisi de piocher 1 carte: {}",
+                        summary.getDrawnCards().get(0).getName()
+                );
             } else if (!player.isDeadForThisTurn()) {
-                System.out.println("Il a choisi de prendre 2 pieces, ce qui l'amene a: " + player.getCash() + " pieces. (après achat de la citadelle si il y a eu)");
+                GAMEPLAY_LOGGER.log(Level.INFO,
+                        "Il a choisi de prendre 2 pieces, ce qui l'amene a: {} pièces. (après achat de la citadelle si il y a eu)",
+                        player.getCash());
             }
         }
 
         if(summary.hasBoughtDistricts()) {
-            System.out.println("Il a acheté cette/ces carte(s):");
-            System.out.println(getDescriptionOfCards(summary.getBoughtDistricts()));
+            GAMEPLAY_LOGGER.info("Il a acheté cette/ces carte(s):");
+            GAMEPLAY_LOGGER.log(Level.INFO, "{}",getDescriptionOfCards(summary.getBoughtDistricts()));
         }
         if(summary.hasFinishDuringTurn()) {
-            System.out.println("Il est le premier premier à finir, car il possède dans sa cité " + GameLogicManager.NUMBER_OF_DISTRICTS_TO_WIN + " citadelles.\n");
-            System.out.println("Voici sa cité");
-            System.out.println(getDescriptionOfCards(player.getCity()));
+            GAMEPLAY_LOGGER.log(Level.INFO, "Il est le premier premier à finir, car il possède dans sa cité {} citadelles.\n",
+                    GameLogicManager.NUMBER_OF_DISTRICTS_TO_WIN
+            );
+            GAMEPLAY_LOGGER.info("Voici sa cité");
+            GAMEPLAY_LOGGER.log(Level.INFO, "{}",getDescriptionOfCards(player.getCity()));
         }
         if(summary.hasUsedMerveillePower()){
-            System.out.println("Des pouvoirs de merveilles sont actifs. Les voici:");
+            GAMEPLAY_LOGGER.info("Des pouvoirs de merveilles sont actifs. Les voici:");
             for(String city: summary.getUsedMerveilles()){
-                System.out.println(city);
+                GAMEPLAY_LOGGER.info(city);
             }
         }
-        System.out.println("\n");
+        GAMEPLAY_LOGGER.info("\n");
     }
 
 
@@ -164,16 +201,22 @@ public class GameOutputManager {
      * @param player joueur
      */
     public void describePlayerState(Player player) {
-        System.out.print("Joueur " + player.getBotLogicName()+player.getStrategyName());
-        System.out.println(" joue son tour, en tant que " + player.getRole().name() + "("+player.getRole().ordinal()+"-" + player.getRole().getColor().name()+").");
-        System.out.println("Il possede actuellement " + player.getCash() + " pieces, et ces cartes en main: ");
-        System.out.println(getDescriptionOfCards(player.getCardsInHand()));
+        GAMEPLAY_LOGGER.log(Level.INFO,
+                "Joueur {}{}",new Object[]{player.getBotLogicName(), player.getStrategyName()}
+        );
+        GAMEPLAY_LOGGER.log(Level.INFO,
+                "joue son tour, en tant que {} ({}-{}).",
+                new Object[]{player.getRole().name(), player.getRole().ordinal(), player.getRole().getColor().name()}
+        );
+        GAMEPLAY_LOGGER.log(Level.INFO,
+                "Il possede actuellement {} pieces, et ces cartes en main:", player.getCash());
+        GAMEPLAY_LOGGER.log(Level.INFO, "{}",getDescriptionOfCards(player.getCardsInHand()));
         if(player.hasEmptyCity()) {
-            System.out.println("Il n'a rien dans sa cité.");
+            GAMEPLAY_LOGGER.info("Il n'a rien dans sa cité.");
         }
         else {
-            System.out.println("Sa cité contient: ");
-            System.out.println(getDescriptionOfCards(player.getCity()));
+            GAMEPLAY_LOGGER.info("Sa cité contient: ");
+            GAMEPLAY_LOGGER.log(Level.INFO, "{}",getDescriptionOfCards(player.getCity()));
         }
     }
 
